e428084a69edf7e26a460b903ff3ebef33cfd2b1
Dzhemile-dzh <dzhemile_ahmet@abv.bg>
2025-07-24 23:28:03 +0300

    Update models to use factory

--- app/Models/Booking.php
+++ app/Models/Booking.php
@@ -3,8 +3,20 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Booking extends Model
 {
-    //
+    use HasFactory;
+
+public function room()
+{
+    return $this->belongsTo(Room::class);
+}
+
+public function guests()
+{
+    return $this->belongsToMany(Guest::class);
+}
+
 }
